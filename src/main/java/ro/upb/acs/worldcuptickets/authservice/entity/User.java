f96c94e07b1376ac2fa27915731e8646e0cedcfb
Alexandru Ciornei <alexstefciornei22@gmail.com>
2025-05-17 15:44:47 +0300

    add jpa, flyway and basic user entity

---
+++ src/main/java/ro/upb/acs/worldcuptickets/authservice/entity/User.java
@@ -0,0 +1,30 @@
+package ro.upb.acs.worldcuptickets.authservice.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
